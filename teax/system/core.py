f4f92780bb932fbbfb6f804bea537be0d9e8ed1b
Maciej A. Czyzewski <mail@maciejczyzewski.me>
2016-02-07 16:04:23 +0100

    [added]: watch option to core

--- teax/system/core.py
+++ teax/system/core.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
 import shutil
 import operator
 import subprocess
 
 from distutils.dir_util import copy_tree
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
 
 from teax import __version__, conf, tty, TEAX_WORK_PATH
 from teax.utils.trash import show_messages
@@ -41,6 +44,7 @@ class CoreController:
     def __init__(self):
         self.shelf = {'filename': None, 'engine': None}
         self.basename = ''
+        self.pdf = True
         self.queue = []
 
     def mount(self, filename):
@@ -59,6 +63,7 @@ class CoreController:
         self.basename = os.path.splitext(filename)[0]
 
     def build(self, pdf=True):
+        self.pdf = pdf
         if self.DEBUG:
             self.__show_debug()
         tty.section('BUILD')
@@ -68,7 +73,7 @@ class CoreController:
             plugins.provide(self.shelf['filename'], plugin)
             engines.provide(self.shelf['filename'], self.shelf['engine'])
             engines.provide(self.shelf['filename'], self.shelf['engine'])
-        if pdf:
+        if self.pdf:
             shutil.copy(self.basename + '.pdf', TEAX_WORK_PATH)
         os.chdir(TEAX_WORK_PATH)
 
@@ -78,6 +83,8 @@ class CoreController:
         latex_filter = LaTeXFilter()
         latex_filter.feed(open(logfile, 'rt').read())
         show_messages(latex_filter.get_messages())
+        if conf['build.watch'] and not conf['process.loop']:
+            self.__stay_and_watch()
 
     def __show_debug(self):
         platform = self.__output(['uname', '-mnprs']).strip()
@@ -89,3 +96,45 @@ class CoreController:
         """Returns output for the given shell command."""
         return subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
 
+    def __stay_and_watch(self):
+        process = Observer()
+        handler = CoreHandler()
+        handler.env(self.shelf['filename'], self.pdf)
+        process.schedule(handler, path=TEAX_WORK_PATH)
+        process.start()
+        conf['process.loop'] = True
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            process.stop()
+        process.join()
+
+
+class CoreHandler(PatternMatchingEventHandler):
+    patterns = ["*.tex", "*.bib"]
+
+    def env(self, filename, pdf):
+        self.filename = filename
+        self.pdf = pdf
+
+    def process(self, event):
+        """
+        event.event_type
+            'modified' | 'created' | 'moved' | 'deleted'
+        event.is_directory
+            True | False
+        """
+        tty.echo(
+            "$[BG_CYAN]%s %s$[NORMAL]" %
+            (event.src_path, event.event_type))
+        core = CoreController()
+        core.mount(self.filename)
+        core.build(pdf=self.pdf)
+        core.result()
+
+    def on_modified(self, event):
+        self.process(event)
+
+    def on_created(self, event):
+        self.process(event)
