4ce8f734024cbf591d17201403df67059b15b208
Maciej A. Czyzewski <mail@maciejczyzewski.me>
2016-02-28 16:04:59 +0100

    [fixed]: build handler

--- teax/system/core.py
+++ teax/system/core.py
@@ -16,7 +16,7 @@ from teax.system import engines, plugins
 from teax.messages import T_FILE_NOT_EXISTS
 from teax.system.parser import LaTeXFilter
 
-class CoreController:
+class CoreController(object):
     """
     PREPARE
     ---> rewrite filename -> basename
@@ -42,55 +42,57 @@ class CoreController:
     DEBUG = True
 
     def __init__(self):
-        self.shelf = {'filename': None, 'engine': None}
-        self.basename = ''
-        self.pdf = True
-        self.queue = []
+        self.__shelf = {'filename': None, 'engine': None}
+        self.__basename = ''
+        self.__pdf = True
+        self.__filter = LaTeXFilter()
 
     def mount(self, filename):
         copy_tree(TEAX_WORK_PATH, TEAX_WORK_PATH + '/.teax/')
-
         if not os.path.splitext(filename)[1]:
             filename = os.path.basename(filename) + '.tex'
         if not os.path.isfile(filename):
             tty.errn(T_FILE_NOT_EXISTS % filename)
-        self.shelf['filename'] = filename
-
+        self.__shelf['filename'] = filename
         engines_list = engines.analyze(filename)
         engine = max(engines_list.iteritems(), key=operator.itemgetter(1))[0]
-        self.shelf['engine'] = engine
-
-        self.basename = os.path.splitext(filename)[0]
+        self.__shelf['engine'] = engine
+        self.__basename = os.path.splitext(filename)[0]
 
     def build(self, pdf=True):
-        self.pdf = pdf
+        self.__pdf = pdf
         if self.DEBUG:
             self.__show_debug()
         tty.section('BUILD')
         os.chdir(TEAX_WORK_PATH + '/.teax/')
-        engines.provide(self.shelf['filename'], self.shelf['engine'])
-        for plugin in plugins.analyze(TEAX_WORK_PATH + '/.teax/'):
-            plugins.provide(self.shelf['filename'], plugin)
-            engines.provide(self.shelf['filename'], self.shelf['engine'])
-            engines.provide(self.shelf['filename'], self.shelf['engine'])
-        if self.pdf:
-            shutil.copy(self.basename + '.pdf', TEAX_WORK_PATH)
+        engines.provide(self.__shelf['filename'], self.__shelf['engine'])
+        self.__run_plugins()
+        if self.__pdf:
+            self.__generate_document()
         os.chdir(TEAX_WORK_PATH)
 
     def result(self):
-        basename = self.basename
-        logfile = TEAX_WORK_PATH + '/.teax/' + basename + '.log'
-        latex_filter = LaTeXFilter()
-        latex_filter.feed(open(logfile, 'rt').read())
-        show_messages(latex_filter.get_messages())
+        logfile = TEAX_WORK_PATH + '/.teax/' + self.__basename + '.log'
+        self.__filter.feed(open(logfile, 'rt').read())
+        show_messages(self.__filter.get_messages())
         if conf['build.watch'] and not conf['process.loop']:
             self.__stay_and_watch()
+
+    def __generate_document(self):
+        if os.path.isfile(self.__basename + '.pdf'):
+            shutil.copy(self.__basename + '.pdf', TEAX_WORK_PATH)
+
+    def __run_plugins(self):
+        for plugin in plugins.analyze(TEAX_WORK_PATH + '/.teax/'):
+            plugins.provide(self.__shelf['filename'], plugin)
+            engines.provide(self.__shelf['filename'], self.__shelf['engine'])
+            engines.provide(self.__shelf['filename'], self.__shelf['engine'])
 
     def __show_debug(self):
         platform = self.__output(['uname', '-mnprs']).strip()
         tty.echo("Path:     {0}".format(TEAX_WORK_PATH))
         tty.echo("Platform: {0}".format(platform))
-        tty.echo("Engine:   {0} [{1}]".format(self.shelf['engine'], __version__))
+        tty.echo("Engine:   {0} [{1}]".format(self.__shelf['engine'], __version__))
 
     def __output(self, cmd):
         """Returns output for the given shell command."""
@@ -99,7 +101,7 @@ class CoreController:
     def __stay_and_watch(self):
         process = Observer()
         handler = CoreHandler()
-        handler.env(self.shelf['filename'], self.pdf)
+        handler.env(self.__shelf['filename'])
         process.schedule(handler, path=TEAX_WORK_PATH)
         process.start()
         conf['process.loop'] = True
@@ -114,9 +116,8 @@ class CoreController:
 class CoreHandler(PatternMatchingEventHandler):
     patterns = ["*.tex", "*.bib"]
 
-    def env(self, filename, pdf):
-        self.filename = filename
-        self.pdf = pdf
+    def env(self, filename):
+        self.__filename = filename
 
     def process(self, event):
         """
@@ -130,7 +131,7 @@ class CoreHandler(PatternMatchingEventHandler):
             (event.src_path, event.event_type))
         core = CoreController()
         core.mount(self.filename)
-        core.build(pdf=self.pdf)
+        core.build()
         core.result()
 
     def on_modified(self, event):
